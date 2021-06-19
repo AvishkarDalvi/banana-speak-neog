bc6dc6f5fb175a87ce246344b0e6fc1e48429f53
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2021-06-19 22:32:36 +0530

    feat: text area addded

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
-var uname = prompt("Give me username!");
-alert("This script works! "+uname);
+var btnTranslate=document.querySelector("#btn-translate");
+var txtInput=document.querySelector("#txt-input");
+function clickEventHandler(){
+    console.log(txtInput.value);
+}
+btnTranslate.addEventListener("click",clickEventHandler);

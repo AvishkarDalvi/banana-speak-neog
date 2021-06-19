38427c41521b38696de41ff1e6643f458e79c6c2
AvishkarDalvi <55877429+AvishkarDalvi@users.noreply.github.com>
2021-06-20 01:10:19 +0530

    feat: api integrated and response handled

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
+var serverUrl = "https://api.funtranslations.com/translate/minion.json";
+
+function constructUrl(text) {
+    return `${serverUrl}?text=${text}`;
+}
+
+function errorHandler(error) {
+    console.log("error occured", error);
+    alert("somethhing wrong with the server! try again after some time.");
+}
 
 function clickEventHandler() {
-    outputDiv.innerText = "agagagagaagagg " + txtInput.value;
+    fetch(constructUrl(txtInput.value)).then(response => response.json()).then(json => {
+        outputDiv.innerText = json.contents.translated;
+    }).catch(errorHandler);
 }
 btnTranslate.addEventListener("click", clickEventHandler);
